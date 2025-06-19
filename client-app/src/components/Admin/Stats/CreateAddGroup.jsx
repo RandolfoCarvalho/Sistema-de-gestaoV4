355b2cc6665d0402050b82676afd626c64942ac6
randolfoCarvalho <randolfoirades@gmail.com>
2025-06-19 14:54:32 -0300

    Fix: Botao desabilita quando nao tem adicional no grupo

--- client-app/src/components/Admin/Stats/CreateAddGroup.jsx
+++ client-app/src/components/Admin/Stats/CreateAddGroup.jsx
@@ -322,7 +322,7 @@ const GerenciamentoAdicionais = () => {
                             {/* Rodapé Fixo */}
                             <div className="p-4 border-t border-gray-200 bg-white flex justify-end gap-4 shrink-0">
                                 <button type="button" onClick={cancelarEdicao} className="px-4 py-2 border border-gray-300 rounded-md text-sm font-medium text-gray-700 hover:bg-gray-100">Cancelar</button>
-                                <button type="submit" disabled={isSubmitting} className="px-6 py-2 bg-blue-600 text-white rounded-md text-sm font-medium hover:bg-blue-700 disabled:bg-blue-300 flex items-center gap-2">
+                                <button disabled={isSubmitting || formData.adicionais.length === 0}  className="px-6 py-2 bg-blue-600 text-white rounded-md text-sm font-medium hover:bg-blue-700 disabled:bg-blue-300 flex items-center gap-2">
                                     {isSubmitting ? <Loader2 className="animate-spin" size={16}/> : <Save size={16}/>}
                                     {isSubmitting ? 'Salvando...' : 'Salvar Alterações'}
                                 </button>
